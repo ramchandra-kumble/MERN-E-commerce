1fe89aa62263d3833e2f3a38fdc414cf166beb5d
Shivani Patel <shivani.patel105556@marwadiuniversity.ac.in>
2021-07-01 15:42:21 +0530

    complete redux upto product details

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -1,5 +1,7 @@
-  import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL } from "../constants/productConstants"
-export const productListReducer =(state = {products:[]},action) =>{
+  import { PRODUCT_LIST_REQUEST,
+     PRODUCT_LIST_SUCCESS,
+     PRODUCT_LIST_FAIL,PRODUCT_DETAILS_REQUEST,PRODUCT__DETAILS_SUCCESS,PRODUCT_DETAILS_FAIL, } from "../constants/productConstants"
+export const productListReducer  =(state = {products:[]},action) =>{
 switch(action.type)
 {
     case PRODUCT_LIST_REQUEST:
@@ -13,3 +15,19 @@ switch(action.type)
         }        
 
 }
+
+
+export const producerDetailsReducer  =(state = {product:{ reviews:[]}},action) =>{
+    switch(action.type)
+    {
+        case PRODUCT_DETAILS_REQUEST:
+            return {loading:true,...state}
+        case PRODUCT_DETAILS_SUCCESS:
+            return {loadings:false,product:action.payload}
+        case PRODUCT_DETAILS_FAIL:
+            return {loading:false,error:action.payload}
+                default:
+                    return state
+            }        
+    
+    }
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,65 +1,84 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import Rating from "../components/Rating";
-import product from "../product";
+import React,{useEffect} from "react"
+import { Link } from "react-router-dom"
+import {useDispatch,useSelector} from 'react-redux'
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap"
+import Rating from "../components/Rating"
+import Message from "../components/Message"
+import Loader from "../components/Loader"
+import product from "../product"
+import { listProductDetails } from "../actions/productAction"
+
+
 
 const ProductScreen = ({ match }) => {
+
+  const dispatch =useDispatch()
+  const productDetails=useSelector(state=> state.productDetails)
+  const {loading,error} =productDetails
+useEffect(()=>
+{
+     dispatch(listProductDetails(match.param.id))
+}
+,[dispatch,match])
+
   const prod = product.find((p) => p._id === match.params.id);
   return (
-    <div>
+    <>
       <Link className="btn btn-light my-3" to="/">
         Go Back
       </Link>
-      <Row>
-        <Col md={6}>
-          <Image src={prod.image} alt={prod.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{prod.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating value={prod.rating} text={`${prod.numReviews} reviews`} />
-            </ListGroup.Item>
-            <ListGroup.Item>Price: ${prod.price}</ListGroup.Item>
-            <ListGroup.Item>Description: {prod.description}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <Row>
-                  <Col>Price: </Col>
-                  <Col>
-                    <strong>${prod.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Status: </Col>
-                  <Col>
-                    {prod.countInStock > 0 ? "In Stock" : "Out Of Stock"}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Button
-                  className="btn-block"
-                  type="button"
-                  disabled={prod.countInStock === 0}
-                >
-                  Add to Cart
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
-    </div>
+      {loading?<Loader/>:error?<Message variant='danger'>{error}</Message>:(
+            <Row>
+            <Col md={6}>
+              <Image src={prod.image} alt={prod.name} fluid />
+            </Col>
+            <Col md={3}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h3>{prod.name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating value={prod.rating} text={`${prod.numReviews} reviews`} />
+                </ListGroup.Item>
+                <ListGroup.Item>Price: ${prod.price}</ListGroup.Item>
+                <ListGroup.Item>Description: {prod.description}</ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price: </Col>
+                      <Col>
+                        <strong>${prod.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status: </Col>
+                      <Col>
+                        {prod.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button
+                      className="btn-block"
+                      type="button"
+                      disabled={prod.countInStock === 0}
+                    >
+                      Add to Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+      )}
+      
+    </>
   );
 };
 

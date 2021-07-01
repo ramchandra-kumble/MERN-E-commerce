96d2970769763a77bda907d4484abe270f6a108a
Shivani Patel <shivani.patel105556@marwadiuniversity.ac.in>
2021-07-01 11:37:06 +0530

    commit upto message in redux

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,21 +1,42 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
-import Product from "../components/Product";
-import product from "../product";
+import React, { useEffect } from 'react'
+import {useDispatch,useSelector} from 'react-redux'
+import { Row, Col } from "react-bootstrap"
+import Product from "../components/Product"
+import Message from "../components/Message"
+import Loader from "../components/Loader"
+
+import {listProducts} from '../actions/productAction'
+
+
 
 const Homescreen = () => {
+  const dispatch = useDispatch()
+  const productList = useSelector(state =>state.productList)
+  const {loading,error,products } = productList
+  useEffect(() =>{
+      dispatch(listProducts())
+  },[dispatch ])
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {product.map((product) => (
+      {loading ?(
+        <Loader/>
+      )
+      :error ? (
+        <Message variant='danger'>{error}
+
+        </Message>
+      ): <Row>
+        {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <h3>
               <Product product={product} />
             </h3>
           </Col>
         ))}
-      </Row>
+      </Row>}
+      
     </>
   );
 };

7ab956b71abeef9143032d52a7bed7c29dbfe557
Vishal Nair <vishalnair111@gmail.com>
2021-07-10 11:43:02 +0530

    action commit

--- client/src/actions/productAction.js
+++ client/src/actions/productAction.js
@@ -13,7 +13,7 @@ export const listProducts =  ()=> async(dispatch)=>
    try{
        dispatch({type:PRODUCT_LIST_REQUEST})
        const { data } = await axios.get('/api/products')
-console.log(data)
+
        dispatch({
            type: PRODUCT_LIST_SUCCESS,
            payload: data

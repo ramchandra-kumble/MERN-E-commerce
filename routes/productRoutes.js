24804ba0f91ce44f0cf30b94e2f480221348ae2b
vomi-exe <mehtavomi@gmail.com>
2021-07-14 16:16:01 +0530

    routes for create and update product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {
   getProducts,
   getProductById,
-  deleteProduct
+  deleteProduct,
+  updateProduct,
+  createProduct
 } = require("../controllers/productControllers");
 const { protect, admin } = require("../middleware/authMiddleware.js");
 
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById).delete(protect,admin, deleteProduct);
+router.route("/").get(getProducts).post(protect, admin , createProduct);
+router.route("/:id").get(getProductById).delete(protect,admin, deleteProduct).put(protect,admin,updateProduct);
 
 module.exports = router;

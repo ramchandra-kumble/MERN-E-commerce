4a40170eb47d1a42f90ce27c8d3076734757fd71
vomi-exe <mehtavomi@gmail.com>
2021-07-16 22:18:08 +0530

    all bugs removed

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -10,7 +10,7 @@ const {
 const { protect, admin } = require("../middleware/authMiddleware.js");
 
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
-router.route("/myorders").get(protect, getMyOrders);
+router.route("/myorders/items").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
 
